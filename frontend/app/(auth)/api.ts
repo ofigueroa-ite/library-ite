6129a09b05a3d7d27ec8b22689f6721deabcf99e
Oscar Figueroa <fisocodes@gmail.com>
2026-04-06 11:26:32 -0700

    feat: redirect on session expired

--- frontend/app/(auth)/api.ts
+++ frontend/app/(auth)/api.ts
@@ -1,12 +1,33 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { FetchArgs, FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import {
+  BaseQueryFn,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { User } from "../(admin)/users/interfaces/user.interface";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:3000/v1/",
+  credentials: "include",
+});
+
+export const baseQueryWithAuthRedirect: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    window.location.href = "/sign-in";
+  }
+
+  return result;
+};
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/v1/",
-    credentials: "include",
-  }),
+  baseQuery: baseQueryWithAuthRedirect,
   tagTypes: ["Me"],
   endpoints: (builder) => ({
     signIn: builder.mutation({
